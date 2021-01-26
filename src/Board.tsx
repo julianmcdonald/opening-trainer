d70d8b4853feffbf0573037fd0ec943a5f90f244
Julian McDonald <warcraft_666_2@hotmail.com>
2021-01-26 21:48:29 +1100

    Add Board and Square

---
+++ src/Board.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Board = styled.div`
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto auto;
+    grid-gap 10px;
+    background-color: #d9d9d9;
+`
+
+export default Board;
---
+++ src/Square.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Square = styled.div`
+
+`
+
+export default Square;
